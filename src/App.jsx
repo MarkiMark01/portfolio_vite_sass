c90ddd75e9d63900629d7c9b2129d9952695b32d
MarkiMark01 <markishgroup@gmail.com>
2025-01-18 20:50:34 +0100

    created first block in Main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/pages/header/Header";
+import Main from "./components/pages/main/Main";
 
 function App() {
   return (
     <main>
       <Header />
+      <Main/>
     </main>
   );
 }
--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -1,10 +1,23 @@
 const Main = () => {
-return(
+  return (
     <section>
-        <section>
-            
-        </section>
+      <section>
+        <p>
+          Hi, I'm Markiyan, a Junior Frontend Developer with a passion for
+          creating modern,
+          <br /> user-friendly websites and applications.
+          <br />
+          I’ve worked on several personal projects where I gained hands-on
+          knowledge <br />
+          and improved my problem-solving skills.
+          <br />
+          I'm eager to join a team where I can contribute my skills, continue
+          learning,
+          <br /> and grow professionally.
+        </p>
+        <a href="">CONTACT ME</a>
+      </section>
     </section>
-)
-}
+  );
+};
 export default Main;
