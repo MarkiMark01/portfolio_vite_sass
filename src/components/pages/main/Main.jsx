54b7c6ec1be460bf2b93228a3f95a654ac767b8e
MarkiMark01 <markishgroup@gmail.com>
2025-01-19 19:20:38 +0100

    updated Main page

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
+
 import styles from "./main.module.scss";
 
 const Main = () => {
+  const [classes, setClasses] = useState("");
+
+  useEffect(() => {
+    setClasses("visible");
+  }, []);
+
   return (
-    <section className={styles.main}>
+    <section id="main" className={classes}>
       <section>
         <p className={styles.text}>
-          Hi, I&apos;m <span>Markiyan Marych,</span> <br/>a Junior Frontend Developer with a passion for
-          creating modern, user-friendly websites and applications.
+          Hi, I&apos;m <span>Markiyan Marych,</span> <br />a Junior Frontend
+          Developer with a passion for creating modern, user-friendly websites
+          and applications.
         </p>
-        <a href="" className={styles.btn}>CONTACT ME</a>
+        <a href="" className={styles.btn}>
+          CONTACT ME
+        </a>
       </section>
       <section>
         <img src="./photo.jpg" alt="My photo" className={styles.photo} />
