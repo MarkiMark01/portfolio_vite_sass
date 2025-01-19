600e558bd489b6d5888a1e6c015e9edcf61539c1
MarkiMark01 <markishgroup@gmail.com>
2025-01-19 20:38:49 +0100

    updated about.scss

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,10 +1,16 @@
-import styles from './about.module.scss';
+import styles from "./about.module.scss";
 
 const About = () => {
-return(
+  return (
     <section id="about">
-        <h2>About</h2>
+      <h2>About</h2>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, quo
+        ex corrupti ullam dolorum animi tempora eius soluta. Corporis culpa
+        earum neque magnam facilis repellat asperiores debitis officiis minima
+        nobis.
+      </p>
     </section>
-)
-}
+  );
+};
 export default About;
