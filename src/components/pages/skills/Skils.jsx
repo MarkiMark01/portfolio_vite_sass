488cbce9fce551de1658dc1b1b83f7a3a06155e9
MarkiMark01 <markishgroup@gmail.com>
2025-01-20 20:14:07 +0100

    created Skills page

---
+++ src/components/pages/skills/Skils.jsx
@@ -0,0 +1,58 @@
+
+
+const Skills = () => {
+return (
+    <section id="skills">
+        <h2>Skills</h2>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+                <img src="" alt="" />
+                <span></span>
+            </div>
+        </div>
+    </section>
+)
+}
+export default Skills;
