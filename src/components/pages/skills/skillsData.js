2061e1df58d224bd173bf20e085312fa8e167df3
MarkiMark01 <markishgroup@gmail.com>
2025-01-21 21:31:59 +0100

    created RestApi.jsx

--- src/components/pages/skills/skillsData.js
+++ src/components/pages/skills/skillsData.js
@@ -7,9 +7,10 @@ import Next from "../../icons/Next";
 import Node from "../../icons/Node";
 import React from "../../icons/React";
 import Redux from "../../icons/Redux";
+import RestApi from "../../icons/RestApi";
 
 export const skillsData = [
-  { Icon: Js, name: "Java Script" },
+  { Icon: Js, name: "JavaScript" },
   { Icon: React, name: "React" },
   { Icon: Next, name: "Next.js" },
   { Icon: Redux, name: "Redux Toolkit" },
@@ -18,5 +19,6 @@ export const skillsData = [
   { Icon: HtIcon, name: "HTML5" },
   { Icon: Css, name: "CSS" },
   { Icon: Git, name: "Git" },
-  { Icon: MongoDb, name: "Mongo Db" },
+  { Icon: MongoDb, name: "MongoDB" },
+  { Icon: RestApi, name: "Rest Api" },
 ];
