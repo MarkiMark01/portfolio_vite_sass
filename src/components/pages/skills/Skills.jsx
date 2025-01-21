0c82149331f0eb339bedfe67e253e7eea49c799a
MarkiMark01 <markishgroup@gmail.com>
2025-01-21 18:24:58 +0100

    created skillsData

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -1,63 +1,25 @@
-import Css from "../../icons/Css";
-import Git from "../../icons/Git";
-import HtIcon from "../../icons/HtIcon";
-import Js from "../../icons/Js";
-import Next from "../../icons/Next";
-import Node from "../../icons/Node";
-import React from "../../icons/React";
-import Redux from "../../icons/Redux";
 import styles from "./skills.module.scss";
+import { skillsData } from "./skillsData";
 
 const Skills = () => {
   return (
     <section id="skills-box">
       <div className={styles.center}>
         <h2>Skills</h2>
-        <div className={styles.skills}>
-          <div className={styles.skill}>
-            <Js />
-            <span>Java Script</span>
-          </div>
-
-          <div className={styles.skill}>
-            <React />
-            <span>React</span>
-          </div>
-
-          <div className={styles.skill}>
-            <Next />
-            <span>Next.js</span>
-          </div>
-          <div className={styles.skill}>
-            <Redux />
-            <span>Redux</span>
-          </div>
-          <div className={styles.skill}>
-            <Node />
-            <span>Node.js</span>
-          </div>
-
-          <div className={styles.skill}>
-            <React />
-            <span>React Native</span>
-          </div>
-
-          <div className={styles.skill}>
-            <HtIcon />
-            <span>HTML5</span>
-          </div>
-
-          <div className={styles.skill}>
-            <Css />
-            <span>CSS</span>
-          </div>
-          <div className={styles.skill}>
-            <Git />
-            <span>Git</span>
-          </div>
+        <div className={styles["skills-container"]}>
+          {skillsData.map((skill, index) => (
+            <div key={index} className={styles.skill}>
+              <skill.Icon className={styles.icon} aria-hidden="true" />
+              <span aria-label={`Skill: ${skill.name}`}>{skill.name}</span>
+            </div>
+          ))}
         </div>
       </div>
     </section>
   );
 };
+
 export default Skills;
+
+
+
---
+++ src/components/pages/skills/skillsData.js
@@ -0,0 +1,20 @@
+import Css from "../../icons/Css";
+import Git from "../../icons/Git";
+import HtIcon from "../../icons/HtIcon";
+import Js from "../../icons/Js";
+import Next from "../../icons/Next";
+import Node from "../../icons/Node";
+import React from "../../icons/React";
+import Redux from "../../icons/Redux";
+
+export const skillsData = [
+  { Icon: Js, name: "JavaScript" },
+  { Icon: React, name: "React" },
+  { Icon: Next, name: "Next.js" },
+  { Icon: Redux, name: "Redux" },
+  { Icon: Node, name: "Node.js" },
+  { Icon: React, name: "React Native" },
+  { Icon: HtIcon, name: "HTML5" },
+  { Icon: Css, name: "CSS" },
+  { Icon: Git, name: "Git" },
+];
