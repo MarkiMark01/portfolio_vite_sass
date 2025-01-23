b7e8c683b828d905faeec60ac3c3654d230e345d
MarkiMark01 <markishgroup@gmail.com>
2025-01-23 20:51:50 +0100

    created Mao.jsx

--- src/components/pages/contacts/Contacts.jsx
+++ src/components/pages/contacts/Contacts.jsx
@@ -1,10 +1,13 @@
 import styles from './contact.module.scss';
+import Phone from '../../icons/Phone';
 
 const Contacts = () => {
 return(
     <section id="contact">
         <h2>Contact me</h2>
-        <div className={styles.contactBox}></div>
+        <div className={styles.contactBox}>
+            
+        </div>
     </section>
 )
 }
