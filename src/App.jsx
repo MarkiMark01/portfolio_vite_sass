214f29a7bb15a3c012eda9e9fc7cfcfb269d2d27
MarkiMark01 <markishgroup@gmail.com>
2025-01-24 07:01:00 +0100

    created footer.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import About from "./components/pages/about/About";
 import Contacts from "./components/pages/contacts/Contacts";
+import Footer from "./components/pages/footer/Footer";
 import Header from "./components/pages/header/Header";
 import Main from "./components/pages/main/Main";
 import Portfolio from "./components/pages/portfolio/Portfolio";
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Portfolio/>
       <Contacts/>
+      <Footer/>
     </main>
   );
 }
