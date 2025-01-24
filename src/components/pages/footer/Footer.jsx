999033048ac18f5e31e73a7ca6fe52fb0c774141
MarkiMark01 <markishgroup@gmail.com>
2025-01-24 06:59:07 +0100

    updated Footer page

--- src/components/pages/footer/Footer.jsx
+++ src/components/pages/footer/Footer.jsx
@@ -1,10 +1,20 @@
-import styles from './footer.module.scss';
+import styles from "./footer.module.scss";
+import In from "../../icons/In";
+import GitHub from "../../icons/GitHub";
 
 const Footer = () => {
-    return(
-        <footer>
-        
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <div className={styles.social}>
+        <a href="https://www.linkedin.com/in/markiyan-marych/" target="_blank">
+          <In />
+        </a>
+        <a href="https://github.com/MarkiMark01" target="_blank">
+          <GitHub />
+        </a>
+      </div>
+      <h2>Thanks for scrolling</h2>
+    </footer>
+  );
+};
 export default Footer;
