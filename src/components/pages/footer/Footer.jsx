90f43265cad6dee31916a8ccc0df32bca155664d
MarkiMark01 <markishgroup@gmail.com>
2025-01-24 07:48:08 +0100

    fixed Footer page

--- src/components/pages/footer/Footer.jsx
+++ src/components/pages/footer/Footer.jsx
@@ -4,14 +4,21 @@ import GitHub from "../../icons/GitHub";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <div className={styles.social}>
-        <a href="https://www.linkedin.com/in/markiyan-marych/" target="_blank">
-          <In />
-        </a>
-        <a href="https://github.com/MarkiMark01" target="_blank">
-          <GitHub />
-        </a>
+        <div className={styles.line}>
+          <div className={styles.links}>
+            <a
+              href="https://www.linkedin.com/in/markiyan-marych/"
+              target="_blank"
+            >
+              <In />
+            </a>
+            <a href="https://github.com/MarkiMark01" target="_blank">
+              <GitHub />
+            </a>
+          </div>
+        </div>
       </div>
       <h2>Thanks for scrolling</h2>
     </footer>
