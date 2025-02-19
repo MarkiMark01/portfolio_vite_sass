5d054b442487460039814664d17ac19d9b95466e
MarkiMark01 <markishgroup@gmail.com>
2025-02-19 16:35:29 +0100

    changed Portfolio and scss

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -1,5 +1,26 @@
+import React, { memo } from "react";
 import styles from "./portfolio.module.scss";
 import { projects } from "./projects";
+
+const PortfolioItem = memo(({ href, img, alt, title, description }) => {
+  return (
+    <li className={styles.portfolioItem}>
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <div className={styles.portfolioElem}>
+          <div className={styles.portfolioInner}>
+            <div className={styles.portfolioFront}>
+              <img src={img} alt={alt} loading="lazy" width="300" height="200" />
+            </div>
+            <div className={styles.portfolioBack}>
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+});
 
 const Portfolio = () => {
   return (
@@ -7,21 +28,14 @@ const Portfolio = () => {
       <h2 className={styles.h2}>Portfolio</h2>
       <ul className={styles.portfolioList}>
         {projects.map(({ href, img, alt, title, description }) => (
-          <li key={title} className={styles.portfolioItem}>
-            <a href={href} target="_blank" rel="noopener noreferrer">
-              <div className={styles.portfolioElem}>
-                <div className={styles.portfolioInner}>
-                  <div className={styles.portfolioFront}>
-                    <img src={img} alt={alt} loading="lazy" />
-                  </div>
-                  <div className={styles.portfolioBack}>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
+          <PortfolioItem
+            key={title}
+            href={href}
+            img={img}
+            alt={alt}
+            title={title}
+            description={description}
+          />
         ))}
       </ul>
     </section>
@@ -29,6 +43,7 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
 
 
 
